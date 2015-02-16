9c2f68654ad4960982bb3d76890edd86213aeb56
Diego Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2015-02-16 10:52:57 -0600

    Added global pin out list

--- uart/python/read.py
+++ uart/python/read.py
@@ -4,33 +4,30 @@ import re
 import RPi.GPIO as gpio
 
 on_regex = re.compile(r'^\s*\b\d{1,2}\b(?:\s*\,?\s*\b\d{1,2}\b)*\s*$')
-avaliable_pins_r2 = [18,23,24,25,8,7,4,17,27,22,10,9,11]
-avaliable_pins_r3 = [18,23,24,25,8,7,12,16,20,21,4,17,27,22,10,9,11,5,6,13,19,26]
-avaliable_pins = []
+global avaliable_pins_r2; avaliable_pins_r2 = [18,23,24,25,8,7,4,17,27,22,10,9,11]
+global avaliable_pins_r3; avaliable_pins_r3 = [18,23,24,25,8,7,12,16,20,21,4,17,27,22,10,9,11,5,6,13,19,26]
+global avaliable_pins; avaliable_pins = []
+
+def turn_on_all(gpio_pins):
+
+  ## Turn off everything
+  print avaliable_pins
+  for pin in avaliable_pins:
+    gpio.output(pin,gpio.LOW)
+  for pin in gpio_pins:
+    gpio.output(pin,gpio.HIGH)
 
 def gpio_setup():
   gpio.setmode(gpio.BCM)
 
   if gpio.RPI_REVISION == 2:
     avaliable_pins = avaliable_pins_r2
+    print avaliable_pins
   else:
     avaliable_pins = avaliable_pins_r3
  
   for pin in avaliable_pins:
     gpio.setup(pin,gpio.OUT)
-
-def turn_on_all(gpio_pins):
-
-  ## Turn off everything
-  for pin in avaliable_pins:
-    gpio.output(pin,gpio.LOW)
-  for pin in gpio_pins:
-    gpio.output(pin,gpio.HIGH)
-
-# def turn_on_all_but(gpio_pins):
-
-# def flash_error():
-
 
 def evaluate_arguments(argv):
   if (on_regex.search(argv) is not None):
