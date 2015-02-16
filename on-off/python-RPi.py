5c72ce886fcfe7f9b49332c3f593189dc3d59249
Diego Aguilar <diegoaguilaraguilar@gmail.com>
2015-02-16 11:24:54 -0600

    Added delay time argument

--- on-off/python-RPi.py
+++ on-off/python-RPi.py
@@ -6,16 +6,16 @@ def nodelay():
 def delay():
 	import time
 	while True:
-		time.sleep(1)
+		time.sleep(DELAY)
 		g.output(PIN,1)
-		time.sleep(1)
+		time.sleep(DELAY)
 		g.output(PIN,0)
 
 import RPi.GPIO as g
 import sys
 
 PIN = int(sys.argv[1])
-
+DELAY = int(sys.argv[sys.argv.length])
 g.setmode(g.BCM)
 g.setup(PIN,g.OUT)
 
