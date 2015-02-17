88d38e9b3dacb59545d73bf2fb9efaa8e03630ea
Diego Aguilar <diegoaguilaraguilar@gmail.com>
2015-02-17 12:31:00 -0600

    Interrupts

--- interrupts/py.py
+++ interrupts/py.py
@@ -12,7 +12,7 @@ g.setup(pin1,g.OUT)
 g.setup(pin2, g.IN, pull_up_down=g.PUD_DOWN)
 
 try:
-  g.add_event_detect(pin2, g.RISING, callback=pushNotification)
+  g.add_event_detect(pin2, g.BOTH, callback=pushNotification)
 
   while True:
     sleep(0.5)
