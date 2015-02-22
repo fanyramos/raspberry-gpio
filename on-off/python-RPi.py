fbd9c119738a7a2951feabe8942fc2a9a543922a
Diego Aguilar <diegoaguilaraguilar@gmail.com>
2015-02-22 00:25:59 -0600

    Fixed function declaration before call

--- on-off/python-RPi.py
+++ on-off/python-RPi.py
@@ -1,18 +1,3 @@
-import RPi.GPIO as g
-import time
-import sys
-
-PIN = int(sys.argv[1])
-
-g.setmode(g.BCM)
-g.setup(PIN,g.OUT)
-
-if "delay" in sys.argv:
-	delay()
-else:
-	nodelay()
-
-
 def nodelay():
 	g.output(PIN,1)
 	g.output(PIN,0)
@@ -24,3 +9,16 @@ def delay():
 		g.output(PIN,1)
 		time.sleep(1)
 		g.output(PIN,0)
+
+import RPi.GPIO as g
+import sys
+
+PIN = int(sys.argv[1])
+
+g.setmode(g.BCM)
+g.setup(PIN,g.OUT)
+
+if "delay" in sys.argv:
+	delay()
+else:
+	nodelay()
