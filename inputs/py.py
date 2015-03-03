8762c987af7e22ad35d406c595865429823d8c9a
Diego Aguilar <diegoaguilaraguilar@gmail.com>
2015-03-02 23:04:19 -0600

    Added bad coded gpio input reading

---
+++ inputs/py.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as gpio
+import sys
+import time
+
+gpio.setmode(g.BCM)
+
+## lsb pins
+g.setup(21,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(20,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(16,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(26,g.IN, pull_up_down=g.PUD_DOWN)
+
+
+## msb pins
+g.setup(19,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(13,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(6,g.IN, pull_up_down=g.PUD_DOWN)
+g.setup(5,g.IN, pull_up_down=g.PUD_DOWN)
+
+
+while True:
+  print "[", g.input(21), g.input(20), g.input(16), g.input(26), g.input(19), g.input(13), g.input(6), g.input(5), "]"
+  time.sleep()
+
+g.cleanup(0.5)
+
