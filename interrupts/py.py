8eaa84f1ec4eaa585f60606a8ef1bcb7fa2bcbfd
Diego Aguilar <diegoaguilaraguilar@gmail.com>
2015-03-05 20:43:25 -0600

    First RPi interrupts test

--- interrupts/py.py
+++ interrupts/py.py
@@ -1,1 +1,13 @@
 import RPi.GPIO as g
+
+def pushNotification(pin,value):
+  print pin, value
+
+g.setmode(g.BCM)
+g.setup(2, g.IN, pull_up_down=g.PUD_UP)
+try:
+  g.add_interrupt_callback(2, pushNotification, edge='rising',  pull_up_down=g.PUD_OFF, threaded_callback=True, debounce_timeout_ms=None)
+  g.wait_for_interrupts(threaded=True)
+except KeyboardInterrupt:
+  g.clean
+g.clean
